eb935dd19fc8f127a22be8b09e0ad13f6f3e8eda
Kristina <codingkris@yahoo.com>
2020-05-21 13:50:51 +0300

    Styles added

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,51 @@
 <template>
-  <div>
+  <div class="container">
+    <div class="row mt-4">
+      <div class="col text-center">
+        <h1 class="text-light">Markdown Preview</h1>
+      </div>
+    </div>
+
+    <div class="row mt-4">
+      <div class="col-md-6">
+        <h4 class="text-light">Markdown</h4>
+        <textarea 
+          class="info"
+          v-model="markdown"
+        />
+      </div>
+      <div class="col-md-6">
+        <h4 class="text-light">Preview</h4>
+        <div class="info">
+          {{ markdown }}
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-  }
-}
+  data() {
+    return {
+      markdown: '',
+    };
+  },
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+textarea {
+  resize: none;
+  border: 2px dashed orange;
+  outline: none;
+}
+.info {
+  height: 400px;
+  width: 100%;
+  border-radius: 5px;
+  background-color: rgba(255, 255, 255, 0.9);
 }
 </style>
